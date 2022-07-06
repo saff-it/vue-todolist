a01c3e5da3b1ba6ffd6dfa134396aea5bfe77889
saff-it <saff_it@ymail.com>
2022-07-06 12:49:27 +0200

    Milestone 1 js+html set up

--- js/script.js
+++ js/script.js
@@ -5,8 +5,22 @@ const app = new Vue (
 
         data: {  
 
-            
-            
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ],
+
+                       
         },
 
         methods: {
